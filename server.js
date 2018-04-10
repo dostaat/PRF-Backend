7e5c73ebb238a90e79ce1b0dfd7649de63e30ed4
ZooLee <zzllee92@gmail.com>
2018-04-10 12:44:28 +0200

    access control

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ passport.use('login', new LocalStrategy.Strategy(function(username, password, do
     });
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(expressSession({ secret: 'thegreatandsecretshow' }));
 app.use(passport.initialize());
 app.use(passport.session());
