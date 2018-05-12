d1dd48373a9209baf907d8a007d5d855a0ffaeda
mateffykristof <mkristof314@gmail.com>
2018-05-12 22:12:34 +0200

    admin and non admin users have differnt home

--- PRF-Frontend/src/app/home/home.component.ts
+++ PRF-Frontend/src/app/home/home.component.ts
@@ -13,11 +13,12 @@ export class HomeComponent implements OnInit {
     users: User[] = [];
  
     constructor(private userService: UserService) {
-        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));        
     }
  
     ngOnInit() {
         this.loadAllUsers();
+        this.changeButton();
     }
  
     deleteUser(_id: string) {
@@ -26,6 +27,19 @@ export class HomeComponent implements OnInit {
  
     private loadAllUsers() {
         this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+
+    private changeButton(){
+        if (this.currentUser.role === "player"){
+            (<HTMLInputElement>document.getElementById("AdminButton")).hidden = true;
+            (<HTMLInputElement>document.getElementById("PlayerButton")).hidden = false;
+        } 
+        else{
+            (<HTMLInputElement>document.getElementById("AdminButton")).hidden = false;
+            (<HTMLInputElement>document.getElementById("PlayerButton")).hidden = true;
+            console.log((<HTMLInputElement>document.getElementById("small_text")) );
+            (<HTMLInputElement>document.getElementById("small_text")).textContent = " You logged in as an admin user :)";
+        }
     }
     
 }
