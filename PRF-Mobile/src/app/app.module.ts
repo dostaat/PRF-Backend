ac6e297eebe056a95e32b6a2d8fe19c83234a5bb
szkabel <szkabel94@gmail.com>
2018-05-15 00:02:50 +0200

    Ionic add city storage

--- PRF-Mobile/src/app/app.module.ts
+++ PRF-Mobile/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -25,6 +26,7 @@ import { NewCityPage} from '../pages/new-city/new-city';
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- PRF-Mobile/src/pages/home/home.ts
+++ PRF-Mobile/src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Geolocation } from '@ionic-native/geolocation';
 import { appConfig } from '../../app/app.config';
 import {HttpClient} from "@angular/common/http";
 import { Cities } from '../../_modules/cities';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -12,12 +13,14 @@ import { Cities } from '../../_modules/cities';
 
 export class HomePage implements OnInit {
   gpsPos: any = {};
-  nearbyCities: Cities[];
-  constructor(public navCtrl: NavController, private geolocation: Geolocation, private http: HttpClient) {
+  nearbyCities: Cities[];  
+  selectedCity = null;
+  constructor(public navCtrl: NavController, private geolocation: Geolocation, private http: HttpClient, private storage: Storage) {
     console.log(http);
   }  
 
   ngOnInit() {
+    
     this.geolocation.getCurrentPosition().then((resp) => {
       this.gpsPos.x_coord = resp.coords.latitude;
       this.gpsPos.y_coord = resp.coords.longitude;      
@@ -28,9 +31,16 @@ export class HomePage implements OnInit {
      }).catch((error) => {
        console.log('Error getting location', error);
      });
+
+     this.storage.get("selectedCity").then( (selectedCity) => {
+      this.selectedCity = selectedCity;
+      });
   }
 
   itemSelected(city) {
-    console.log("Selected city: " + city.name);
+    console.log("set storage city");
+    this.storage.set("selectedCity",city.name);
+    this.selectedCity = city.name;
+    console.log(city.name);
   }
 }
